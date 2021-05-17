13cc7fa04689c311ba7e2d8af2adb8a3f5be5aad
Melisa KMNV <melisakmnv@gmail.com>
2021-05-17 19:42:06 +0200

    search bar done

--- public/javascript/join.js
+++ public/javascript/join.js
@@ -6,3 +6,24 @@ const container = document.querySelectorAll(".container");
 
 // signup_btn.addEventListener('click', () => container.classList.add('right_panel_active'));
 // signin_btn.addEventListener('click', () => container.classList.remove('right_panel_active'));
+
+
+
+
+// ================= SEARCH BAR ================= //
+
+const icon = document.querySelector('.icon')
+const search = document.querySelector('.search');
+const clear = document.querySelector('.clear')
+let new_input = document.querySelector('#new_input');
+
+// icon.addEventListener("click", () => search.classList.toggle('active'));
+clear.addEventListener("click", () => new_input.value = '');
+
+
+// setInterval(() => {
+//     search.classList.remove('active')
+// }, 7000);
+
+
+//  Add Event => enter button => search data
