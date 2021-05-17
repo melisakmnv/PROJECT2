264c3ad53b273c63f62f9dc2b53db9299d00655c
Melisa KMNV <melisakmnv@gmail.com>
2021-05-17 21:02:04 +0200

    modified authRoute : verify username

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,78 +3,92 @@ const router = new express.Router();
 const bcrypt = require('bcrypt');
 const UserModel = require('../models/userModel');
 
+// ========= NEED TO PUT FLASH MESSAGE ========= //
+
+// ======= GET - SIGNIN ======= //
 router.get('/signin', (req, res) => {
   res.render('auth/signin.hbs');
 });
 
+// ======= POST - SIGNIN ======= //
 
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
   const foundUser = await UserModel.findOne({ email: email });
   if (!foundUser) {
-      res.redirect("/auth/signin")
+    res.redirect('/auth/signin');
 
-      // ----- postman test ----- //
+    // ----- postman test ----- //
     // return res.status(400).json({ errors: [{ msg: 'No user' }] }); // to see on the console // Postman
-      // ------- need flash message ------- //
+
+    // ------- FLASH MESSAGE GOES HERE ------- //
   } else {
     const isSamePassword = bcrypt.compareSync(password, foundUser.password);
     if (!isSamePassword) {
-        res.redirect("/auth/signin")
+      res.redirect('/auth/signin');
 
-        // ----- postman test ----- //
-    //   return res
-    //     .status(400)
-    //     .json({ errors: [{ msg: 'Password is not correct' }] });
-      // ------- need flash message ------- //
+      // ----- postman test ----- //
+      //   return res
+      //     .status(400)
+      //     .json({ errors: [{ msg: 'Password is not correct' }] });
+
+      // ------- FLASH MESSAGE GOES HERE ------- //
     } else {
-
-        // ----- postman test ----- //
-    //   res.send('Yayyyy');
-      // ------- need flash message ------- //
-    res.redirect("/")
+      // ----- postman test ----- //
+      //   res.send('Yayyyy');
+      /// ------- FLASH MESSAGE GOES HERE ------- //
+      res.redirect('/');
     }
   }
 });
 
-//POST --- Add new user //
+// ======= GET - SIGNUP ======= //
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup.hbs');
 });
 
-// POST - ADD new user => Need to be modified => Check email, check password
+// ======= POST - SIGNUP - Add new user ======= //
 
 router.post('/signup', async (req, res, next) => {
   try {
     const newUser = { ...req.body };
     const foundUser = await UserModel.findOne({ email: newUser.email });
+    const foundUsername = await UserModel.findOne({
+      username: newUser.username,
+    });
 
     if (foundUser) {
-        res.redirect("/auth/signup")
+      res.redirect('/auth/signup');
 
-        // ----- postman test ----- //
-    //   return res
-    //     .status(400)
-    //     .json({ errors: [{ msg: 'This Email is already used' }] });
-    // ------- need flash message ------- //
+      // ----- postman test ----- //
+      // return res.status(400).json({ errors: [{ msg: 'This Email is already used' }] });
 
+      // ------- FLASH MESSAGE GOES HERE ------- //
+    } else if (foundUsername) {
+      res.redirect('/auth/signup');
+      // ----- postman test ----- //
+      // return res
+      //   .status(400)
+      //   .json({ errors: [{ msg: 'username is already used' }] });
+
+      // ------- FLASH MESSAGE GOES HERE ------- //
     } else {
       const hashedPassword = bcrypt.hashSync(newUser.password, 10);
       newUser.password = hashedPassword;
       await UserModel.create(newUser);
-      res.redirect("/auth/signin")
+      res.redirect('/auth/signin');
 
       // ----- postman test ----- //
-    //   res.send('You are registered !');
-      // ------- need flash message ------- //
+      // res.send('You are registered !');
 
+      // ------- FLASH MESSAGE GOES HERE ------- //
     }
   } catch (err) {
     console.error(err);
     return res.status(400).json({ errors: [{ msg: 'problem' }] });
-      // ------- need flash message ------- //
+
+    // ------- FLASH MESSAGE GOES HERE ------- //
   }
 });
 module.exports = router;
-
