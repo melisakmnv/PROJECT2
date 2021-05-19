32624aa9c8b9a04c22fbf4856c05e5c52ce4a230
Alice LEFEVRE <alefevre.contact@gmail.com>
2021-05-19 18:08:12 +0200

    seed modif + dashboard

--- bin/seed.user.js
+++ bin/seed.user.js
@@ -1,19 +1,30 @@
 require("../config/mongodb")
 const UserModel = require("../models/userModel");
 
-const users = [{
-    firstname: "Melisa",
-    lastname: "CHA",
-    location: "Paris",
-    email: "melisahh@gmail.com",
-    password: "1234"
-},
+const users = [
+// {
+//     firstname: "Melisa",
+//     lastname: "CHA",
+//     location: "Paris",
+//     email: "melisahh@gmail.com",
+//     password: "1234"
+// },
+// {
+//     firstname: "Eric",
+//     lastname: "CHASUN",
+//     location: "Paris",
+//     email: "ericSH@gmail.com",
+//     password: "3333"
+// },
 {
-    firstname: "Eric",
-    lastname: "CHASUN",
-    location: "Paris",
-    email: "ericSH@gmail.com",
-    password: "3333"
+    username: "alice",
+    email: "o@o",
+    password: "$",
+    role: "admin",
+    avatar: "https://i.pinimg.com/originals/62/cf/de/62cfdef6cb05772d1c4c5e3882839238.png",
+    description: "Team admin",
+    city: "Paris",
+    wishlist: ""
 }
 ];
 
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -7,34 +7,30 @@ const ActivityModel = require("./../models/activityModel");
 //DASHBOARD
 router.get("/", async (req, res, next) => {
   const user = await UserModel.findById(req.session.currentUser._id).populate("wishlist");
+  console.log(user)
   res.render("dashboard/dashboard.hbs", { user });
 });
 
 //WISHLIST
 router.get("/mywishlist", async (req, res, next) => {
-    const users = await UserModel.find();
-    const destinations = await ActivityModel.find();
-    res.render("dashboard/wishlist.hbs", { users, destinations });
-  });
+  const destinations = await UserModel.findById(req.session.currentUser._id).populate("wishlist");
+  res.render("dashboard/wishlist.hbs", { destinations });
+});
 
 // Destination ADD
-
 router.get("/destination_add", (req, res) => {
   res.render("dashboard/destination_add.hbs");
 });
 
 router.post("/destination_add", (req, res, next) => {
-  
-    ActivityModel.create(req.body)
-      .then((dbResult) => {
-        console.log(dbResult);
-        res.redirect("/dashboard");
-      })
-      .catch((err) => {
-        res.render("dashboard/destination_add.hbs");
-      });
-  });
-
-
+  ActivityModel.create(req.body)
+    .then((dbResult) => {
+      console.log(dbResult);
+      res.redirect("/dashboard");
+    })
+    .catch((err) => {
+      res.render("dashboard/destination_add.hbs");
+    });
+});
 
 module.exports = router;
